29a3682632ace3b284cbdf9fc96b05b11ea40a51
Leyna Varinia <108304485+LeynaV@users.noreply.github.com>
2022-12-01 23:07:00 -0400

    Agregar archivos de proyecto.

---
+++ ConsoleApp11/Program.cs
@@ -0,0 +1,68 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+//LECTURA DE DATOS (ESTRUCTURA SECUENCIAL)
+
+// Leer 2 números e imprimir el resultado de su suma
+/*int a, b, sum = 0;
+Console.WriteLine("Ingresa el primer numero");
+a = int.Parse(Console.ReadLine());
+Console.WriteLine("Ingresa el primer numero");
+b = int.Parse(Console.ReadLine());
+sum = a + b;
+Console.WriteLine("la suma de los dos numero es " + sum);*/
+
+//Leer 2 números e imprimir el resultado de su resta.
+/*int a, b, sum = 0;
+Console.WriteLine("Ingresa el primer numero");
+a = int.Parse(Console.ReadLine());
+Console.WriteLine("Ingresa el primer numero");
+b = int.Parse(Console.ReadLine());
+sum = a - b;
+Console.WriteLine("la resta de los dos numero es " + sum);*/
+
+//Leer 2 números e imprimir el resultado de su producto.
+
+/*int a, b, sum = 0;
+Console.WriteLine("Ingresa el primer numero");
+a = int.Parse(Console.ReadLine());
+Console.WriteLine("Ingresa el primer numero");
+b = int.Parse(Console.ReadLine());
+sum = a * b;
+Console.WriteLine("El producto de los dos numero es " + sum);*/
+
+//Leer 2 números e imprimir el resultado de su división.
+
+/*int a, b, sum = 0;
+Console.WriteLine("Ingresa el primer numero");
+a = int.Parse(Console.ReadLine());
+Console.WriteLine("Ingresa el primer numero");
+b = int.Parse(Console.ReadLine());
+sum = a / b;
+Console.WriteLine("la divición de los dos numero es " + sum);*/
+
+/*Leer 2 números e imprimir las 6 operaciones fundamentales (Suma, resta, multiplicación,
+división entera, división real, modulo).*/
+
+/*int a, b, sum = 0, res = 0, prod = 0, mod = 0;
+float divE = 0, divR = 0;
+Console.WriteLine("Ingresa el primer numero");
+a = int.Parse(Console.ReadLine());
+Console.WriteLine("Ingresa el primer numero");
+b = int.Parse(Console.ReadLine());
+sum = a + b;
+res = a - b;
+prod = a * b;
+divE = a / b;
+divR = a / b;
+mod = a % b;
+Console.WriteLine("La suma de los numero es: " + sum);
+Console.WriteLine("La resta de los numero es: " + res);
+Console.WriteLine("La multiplicación de los numero es: " + prod);
+Console.WriteLine("La divicion entera de los numero es: " + divE);
+Console.WriteLine("La Divicion real de los numero es: " + divR);
+Console.WriteLine("La modulo de los numero es: " + mod);/*
+
+    //LECTURA DE DATOS 
+// Leer 2 numeros y imprimir si es positivo o negativo 
+
+
